86e88d5cebb2340316af81ebc15703d4e84984de
Glennie Rousseva <glennierousseva@Glennies-MacBook-Pro.local>
2019-01-24 10:28:15 -0800

    updated routes, added templates, and learn more and sign up button

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
-from flask import render_template
-from app import app
+from flask import Flask, render_template
 
-@app.route('/')
-@app.route('/index')
+app = Flask(__name__)
+
+@app.route("/")
 def index():
-	return render_template('index.html', title='Home', hw= "Hello World")
+  return render_template("index.html")
+
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
