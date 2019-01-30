4dd5c5684278ce651292a26604a9a1b93677a5b6
Glennie Rousseva <glennierousseva@169-231-219-49.wireless.ucsb.edu>
2019-01-30 14:45:10 -0800

    connecting app to postgresql database

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from models import db, User
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:trader@localhost/db'
+db.init_app(app)
+
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
