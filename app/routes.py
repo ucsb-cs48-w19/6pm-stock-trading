14aeb5b8a4fc6a751ac6b6afea6a55e16bbf0dd3
Sassan Hashemi <sassan@gmail.com>
2019-01-31 18:43:21 -0800

    fix formatting differences

--- app/routes.py
+++ app/routes.py
@@ -4,22 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+  return render_template("index.html")
 
 @app.route("/about")
 def about():
-	return render_template("about.html")
+  return render_template("about.html")
 
 @app.route("/dashboard")
 def dashboard():
-	return render_template("dashboard.html")
+  return render_template("dashboard.html")
 
 @app.route("/personal-info")
 def personal_info():
-	return render_template("personal-info.html")
+  return render_template("personal-info.html")
 
 
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+  app.run(debug=True)
