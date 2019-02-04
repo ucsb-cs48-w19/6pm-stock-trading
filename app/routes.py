f8bcb2497f9b4709d8633e9cdd98a1d46f87250d
Glennie Rousseva <glennierousseva@169-231-221-175.wireless.ucsb.edu>
2019-02-04 14:39:36 -0800

    added signup page, form for signup, and connected database to signup

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User
+from forms import SignupForm
 
 app = Flask(__name__)
 
@@ -20,24 +21,30 @@ def about():
 def dashboard():
   return render_template("dashboard.html")
 
-
 @app.route("/personal-info")
 def personal_info():
   return render_template("personal-info.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-    
-@app.route("/login", methods = ['GET', 'POST'])
-def authenticate_user():
-	print("Login Processing ") 
-	return render_template("index.html")
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+  if 'email' in session:
+    return redirect(url_for('dashboard'))
 
-	#process user data from the post request
+  form = SignupForm()
 
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
 
+      session['email'] = newuser.email
+      return redirect(url_for('dashboard'))
 
+  elif request.method == "GET":
+    return render_template('signup.html', form=form)
 
 
 if __name__ == "__main__":
