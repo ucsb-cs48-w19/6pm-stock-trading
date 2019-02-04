d81e33dee348f03a1a9e8b60ffb1e359e60cc5c6
Glennie Rousseva <glennierousseva@Glennies-MacBook-Pro.local>
2019-02-04 11:18:56 -0800

    connected postgres and put users model

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:trader@localhost/db'
+db.init_app(app)
+
+app.secret_key = "development-key"
+
 @app.route("/")
 def index():
   return render_template("index.html")
