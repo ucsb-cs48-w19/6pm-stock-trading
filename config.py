b87c6e853e745662aa72ab1a44341ef0af7f061b
maxtong9 <maxginier1@gmail.com>
2019-02-12 01:27:55 -0800

    biggest change ever. not sure where to begin.. changed routes to py. refactored a lot of the imports between app, models, and manage, delete __init__.py for now, changed password hash length from 54 to 100 to avoid the hashing error, changed a lot of the model code, changed a lot of the new (old routes) app.py code, .......................................

--- config.py
+++ config.py
@@ -2,8 +2,37 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    #Added for postgres hosting?
+    DEBUG = False
+    TESTING = False
+    
+    #^^^
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir,'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    #Change according to tutorial (medium.com)
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+
+
+
+#Added for postgres hosting?
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+#^^^
