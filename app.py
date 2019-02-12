f9eb1bea34738ac8a51f49aaee5f8415565bf939
maxtong9 <maxginier1@gmail.com>
2019-02-12 02:00:11 -0800

    Removed app settings

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 #Added initializations for postgres hosting
-app.config.from_object(os.environ['APP_SETTINGS'])
+#app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
