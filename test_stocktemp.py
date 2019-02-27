735b830fe69c415930b698cd551ade6b8ff290cd
BOBBYUAN <bobyuanwangcheng@gmail.com>
2019-02-26 19:11:37 -0800

    try about

--- test_stocktemp.py
+++ test_stocktemp.py
@@ -1,7 +1,7 @@
 import pytest
 #import os
 #from flask import Flask, render_template, request, session, redirect, url_for, flash
-#from app import about
+from app import about
 
 def test_stocktemp():
-    assert(0 == 0)
+    assert(about() == render_template("about.html"))
