660a79be9d18c9f75738e6ef0497cd03a2299350
BOBBYUAN <bobyuanwangcheng@gmail.com>
2019-02-27 17:27:44 -0800

    Update test_stocktemp.py

--- test_stocktemp.py
+++ test_stocktemp.py
@@ -1,7 +1,7 @@
 import pytest
 #import os
 from flask import Flask, render_template, request, session, redirect, url_for, flash
-from config import fact
+from utility import fact
 
 def test_stocktemp():
     assert fact(10) == pytest.approx(90)
