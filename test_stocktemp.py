44d481ce1e9d3a248040a29e195d16aeb15a2d04
BOBBYUAN <bobyuanwangcheng@gmail.com>
2019-02-27 17:09:38 -0800

    Update test_stocktemp.py

--- test_stocktemp.py
+++ test_stocktemp.py
@@ -1,7 +1,7 @@
 import pytest
 #import os
 from flask import Flask, render_template, request, session, redirect, url_for, flash
-from app import about
+from config import fact
 
-def test_stocktemp_about():
-    assert (about()==render_template("about.html"))
+def test_stocktemp():
+    assert (assert fact(10) == pytest.approx(90))
