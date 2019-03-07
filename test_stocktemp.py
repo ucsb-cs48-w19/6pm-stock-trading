246f5c95015b13fe1e22785d90eabe5d3fcea91c
BOBBYUAN <bobyuanwangcheng@gmail.com>
2019-03-07 11:55:28 -0800

    Update test_stocktemp.py

--- test_stocktemp.py
+++ test_stocktemp.py
@@ -1,6 +1,12 @@
 import pytest
 #import os
 #from flask import Flask, render_template, request, session, redirect, url_for, flash
+from pandas_datareader import data
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import io
+import base64
 from graph import graphMaker
 
 
